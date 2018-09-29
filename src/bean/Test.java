2ebbafed5c7b560ddd538b9a9b7c9660d69cab4c
yang <m18511938625@163.com>
2018-09-29 13:59:46 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/bean/Test.java

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -4,6 +4,7 @@ import static utils.Utils.*;
 public class Test {
 	public static void main(String[] args) {
 		ChessBoard c = ChessBoard.getChessBoard();
+
 		while(true){
 			c.setChess(readPosition(), ChessMan.BLACK);
 			System.out.println(c.getChessBoardArray());
