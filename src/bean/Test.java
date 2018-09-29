f130b7db3ef2103baa3d6fc576d469099cb9976c
yang <m18511938625@163.com>
2018-09-29 12:54:25 +0800

    add a show

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -4,10 +4,11 @@ import static utils.Utils.*;
 public class Test {
 	public static void main(String[] args) {
 		ChessBoard c = ChessBoard.getChessBoard();
-		while(true){
-			c.setChess(readPosition(), ChessMan.BLACK);
-			System.out.println(c.getChessBoardArray());
-		}
-		
+        System.out.println("what the hell");
+        while(true){
+            c.setChess(readPosition(), ChessMan.BLACK);
+            System.out.println(c.getChessBoardArray());
+        }
+
 	}
 }
